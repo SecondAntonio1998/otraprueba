35211d6b4890f39eaaef237bfb77d46dcafb632e
unknown <crashx30@hotmail.com>
2020-10-29 23:06:11 -0600

    nuevodiv

--- parte1.py
+++ parte1.py
@@ -10,9 +10,14 @@ def zelta(a,b):
     multi=a-b
     print("La multiplicacion es",multi)
 
+def kilo(a,b):
+    div=a/b
+    print("La Div es",div)
+
 a=int(input("Ingresa  el primer valor"))
 b=int(input("Ingresa el segundo valor"))
 
 delta(a,b)
 beta(a,b)
 zelta(a,b)
+kilo(a,b)
